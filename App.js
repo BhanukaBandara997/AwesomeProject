47e2cd843880b6f3b472ad7cbb04c4e261c6fe24
BhanukaBandara <bhanuka.2016147@iit.ac.lk>
2019-05-05 20:01:26 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
+
+export default class App extends Component {
+
+  // State variable for the text input
+  state = {
+    places: []
+  }
+
+  placeAddedHander = placeName => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(placeName)
+      };
+    });
+  };
+
+  placeDeleteHandler = index => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter((placeName, i) => {
+          return i !== index;
+        })
+      }
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PlaceInput onPlaceAdded={this.placeAddedHander} />
+        <PlaceList places={this.state.places} onItemDeleted={this.placeDeleteHandler} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    padding: 25
+  }
+});
---
+++ src/components/PlaceInput/PlaceInput.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Button, TextInput } from 'react-native';
+
+class PlaceInput extends Component {
+
+    // State variable for the text input
+    state = {
+        placeName: ""
+    }
+
+    // Method to handle text input value
+    placeNameOnChangeListener = val => {
+        this.setState({
+            placeName: val
+        });
+    };
+
+    placeSubmitHander = () => {
+        if (this.state.placeName.trim() === "") {
+            return;
+        }
+
+        this.props.onPlaceAdded(this.state.placeName);
+    };
+
+    render() {
+        return (
+            <View style={styles.inputContainer}>
+                <TextInput
+                    value={this.state.placeName}
+                    placeholder="An Awesome Place"
+                    onChangeText={this.placeNameOnChangeListener}
+                    style={styles.placeInput} />
+
+                <Button title="Add" style={styles.placeButton} onPress={this.placeSubmitHander} />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    placeInput: {
+        width: "70%"
+    },
+    placeButton: {
+        width: "30%"
+    }
+});
+
+export default PlaceInput;
