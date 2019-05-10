69e00913f6d314cd2492651d10a7bb3d562d9c9b
BhanukaBandara <bhanuka.2016147@iit.ac.lk>
2019-05-11 00:33:31 +0530

    Redux adding

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,6 @@
+import { ADD_PLACE, DELETE_PLACE, DESELECT_PLACE, SELECT_PLACE } from '../actions/actionTypes';
+
+
 const initialState = {
     places: [],
     selectedPlace: null
@@ -6,6 +9,36 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_PLACE:
+            return {
+                ...state,
+                places: state.places.concat({
+                    key: Math.random(),
+                    name: placeName,
+                    image: {
+                        uri: 'https://www.google.com/search?q=background+images&safe=active&tbm=isch&source=iu&ictx=1&fir=V9QSez5mpRnf0M%3A%2CkcdXjkNuF-KTxM%2C_&vet=1&usg=AI4_-kRzfBOeZ1u6wod3DBp0gYg-APb8uA&sa=X&ved=2ahUKEwilreaA0pDiAhWE73MBHWoyCPIQ9QEwCXoECAcQFg#imgrc=RRx0s15l48ONCM:&vet=1'
+                    }
+                })
+            };
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+            };
+        case SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(place => {
+                    return place.key === action.placeKey;
+                })
+            };
+        case DESELECT_PLACE:
+            return {
+                selectedPlace: null
+            };
         default:
             return state;
     }
